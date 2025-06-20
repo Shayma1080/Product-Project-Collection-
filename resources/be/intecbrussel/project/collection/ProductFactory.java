b89f8793449665a19b69c83af28c0a5173c00529
3 Klasses  Update <hajji.chaima@outlook.fr>
2025-06-20 15:49:16 +0200

    update

--- resources/be/intecbrussel/project/collection/ProductFactory.java
+++ resources/be/intecbrussel/project/collection/ProductFactory.java
@@ -1,16 +1,24 @@
 package be.intecbrussel.project.collection;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ProductFactory {
+    Random random = new Random();
     private ProductSorter productSorter;
-    private String[] foodProducts;
-    private String[] healthProducts;
-    private  String[] electricProduct;
+    private String[] foodProducts = {"Apple", "Steak", "Potatoes", "Salad", "Pear"};
+    private String[] healthProducts = {"Face cream", "Vitamins", "Eye drops", "Toothpaste", "Shampoo"};
+    private  String[] electricProduct  = {"Hair dryer", "Vacuum", "Mixer", "Washing machine", "Dishwasher"};
 
     public ProductFactory(ProductSorter productSorter) {
         this.productSorter = productSorter;
     }
 
     public int createProducts(int amount){
+        ArrayList<FoodProduct> food = new ArrayList<>();
+        for(int i= 0 ;i < amount;i++){
+
+        }
 
         return amount;
     }
